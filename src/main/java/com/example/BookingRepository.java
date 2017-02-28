d20dc4518b5fbd529d8eaaba9ea8a1a24f966a1c
ealfageme <e.alfageme@alumnos.urjc.es>
2017-02-28 09:35:42 +0100

    Repositories added

---
+++ src/main/java/com/example/BookingRepository.java
@@ -0,0 +1,7 @@
+package com.example;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookingRepository extends JpaRepository<Booking,Long>{
+
+}
---
+++ src/main/java/com/example/RestaurantRepository.java
@@ -0,0 +1,7 @@
+package com.example;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RestaurantRepository extends JpaRepository<Restaurant,Long>{
+
+}
