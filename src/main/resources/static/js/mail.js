68f762db15fbc09a023ce590fe9f0dcfa6673277
sergiopaniego <sergiopaniegoblanco@gmail.com>
2017-03-07 13:49:51 +0100

    create-drop in DB

--- src/main/resources/static/js/mail.js
+++ src/main/resources/static/js/mail.js
@@ -4,3 +4,18 @@ function sendFuguFeedback() {
 	let body=document.getElementById("message").value;
 	window.location.href = "mailto:feedback@fugu.com?subject=Feedback&body="+body+"%0A%0AMessage written by: "+username+" ("+email+").";
 }
+
+function validateEmail(email) {
+  var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+}
+
+function validate() {
+  var email = $("#email").val();
+  if (validateEmail(email)) {
+    $("#email").css("color", "green");
+  } else {
+    $("#email").css("color", "red");
+  }
+  return false;
+}
