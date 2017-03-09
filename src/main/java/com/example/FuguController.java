0569869146d9c9b3d91e8f26c5bd977f5e421965
ealfageme <e.alfageme@alumnos.urjc.es>
2017-03-09 12:49:02 +0100

    capture data restaurant

--- src/main/java/com/example/FuguController.java
+++ src/main/java/com/example/FuguController.java
@@ -93,6 +93,7 @@ public class FuguController {
 		model.addAttribute("menu", restaurantRepository.findByName(name).getMenus());
 		model.addAttribute("vouchers", restaurantRepository.findByName(name).getVouchers());
 		model.addAttribute("reviews", restaurantRepository.findByName(name).getRestaurantReviews());
+		
 		return "public-restaurant";
 	}
 
@@ -109,12 +110,31 @@ public class FuguController {
 	}
 
 	@RequestMapping("/private-restaurant/{name}")
-	public String privateRestaurant(Model model, @PathVariable String name, @RequestParam(required=false) String type, @RequestParam(required=false) Integer max, @RequestParam(required=false) Integer min, @RequestParam(required=false) String vouchername, @RequestParam(required=false) String voucherdescription,@RequestParam(required=false) String menudescription,@RequestParam(required=false) String menuname,@RequestParam(required=false) Double menuprice) {
+	public String privateRestaurant(Model model, @PathVariable String name, @RequestParam(required=false) String type,
+	@RequestParam(required=false) Integer max, @RequestParam(required=false) Integer min,
+	@RequestParam(required=false) String vouchername, @RequestParam(required=false) String voucherdescription,
+	@RequestParam(required=false) String menudescription,@RequestParam(required=false) String menuname,
+	@RequestParam(required=false) Double menuprice, @RequestParam(required=false)String namerest,
+	@RequestParam(required=false)String location, @RequestParam(required=false)Integer telephone,
+	@RequestParam(required=false)String descriptionrest,@RequestParam(required=false)String emailrest,
+	@RequestParam(required=false)String pwd,@RequestParam(required=false)String confirmpwd) {
 		model.addAttribute("restaurant", restaurantRepository.findByName(name));
 		model.addAttribute("menu", restaurantRepository.findByName(name).getMenus());
 		model.addAttribute("bookings", restaurantRepository.findByName(name).getBookings());
 		model.addAttribute("vouchers", restaurantRepository.findByName(name).getVouchers());
 		model.addAttribute("reviews", restaurantRepository.findByName(name).getRestaurantReviews());
+		if (namerest!=null){
+			Restaurant restaurant = restaurantRepository.findByName(name);
+			restaurant.setName(namerest);
+			restaurant.setAddress(location);
+			restaurant.setDescription(descriptionrest);
+			restaurant.setPhone(telephone);
+			restaurant.setEmail(emailrest);
+			if (pwd.equals(confirmpwd)){
+				restaurant.setPassword(pwd);
+			}
+			restaurantRepository.save(restaurant);
+		}
 		if (vouchername!=null){
 			Voucher voucher= new Voucher(vouchername,voucherdescription,new Date());
 			voucher.setVoucherUsers(userRepository.findByAgeBetween(min,max));
