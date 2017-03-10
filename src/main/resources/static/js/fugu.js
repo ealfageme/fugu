3f7e6ad9b9789e6b6c3fd1d97e36783ce5368107
sergiopaniego <sergiopaniegoblanco@gmail.com>
2017-03-10 18:22:26 +0100

    Validation in sign in

--- src/main/resources/static/js/fugu.js
+++ src/main/resources/static/js/fugu.js
@@ -5,40 +5,102 @@ function sendFuguFeedback() {
 	window.location.href = "mailto:feedback@fugu.com?subject=Feedback&body="+body+"%0A%0AMessage written by: "+username+" ("+email+").";
 }
 
-function validateEmail(email) {
-  var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(email);
-}
 
-function validate() {
-  var email = $("#email").val();
-  if (validateEmail(email)) {
-    $("#email").css("color", "green");
-  } else {
-    $("#email").css("color", "red");
-  }
-  return false;
-}
 
 $('#password_restaurant, #confirm_password_restaurant').on('keyup', function () {
 	if($('#password_restaurant').val().length>=6){
 		if ($('#password_restaurant').val() !== $('#confirm_password_restaurant').val()&&$('#confirm_password_restaurant').val().length>=6) {
 			$('#message_restaurant').html('Passwords not matching').css('color', 'red');
-	    } else{
+			document.getElementById("sendrestaurant").disabled = true;
+			$('#message_restaurant_button').html('Fill the form before register');
+			document.getElementById('sendrestaurant').className = "logInBtn";
+	    } else if($('#confirm_password_restaurant').val().length<6){  	
+	    	document.getElementById("sendrestaurant").disabled = true;
+	    	$('#message_restaurant_button').html('Fill the form before register');
+	    	document.getElementById('sendrestaurant').className = "logInBtn";
+	    }else{
 	    	$('#message_restaurant').html('');
+	    	if($('#restaurantdescription').val().length>0 && $('#restaurantname').val().length>0 && $('#restaurantaddress').val().length>0 
+	    	&& $('#restaurantphone').val().length>0 && $('#restaurantemail').val().length>0){
+	    		if(isNumber($('#restaurantphone').val())){
+	    			document.getElementById('sendrestaurant').className = "btnMore";
+	    			document.getElementById('sendrestaurant').removeAttribute('disabled');
+	    			$('#message_restaurant_button').html('');
+	    		}
+	    			
+	    	}
 	    }
 	}
 });
+
+$('#restaurantphone').on('keyup', function () {
+	if($('#restaurantphone').val().length==0 || !isNumber($('#restaurantphone').val())){
+		document.getElementById("sendrestaurant").disabled = true;
+		$('#message_restaurant_phone').html('Phone must be nuberic').css('color', 'red');
+		$('#message_restaurant_button').html('Fill the form before register');
+		document.getElementById('sendrestaurant').className = "logInBtn";
+	}else{
+		$('#message_restaurant_phone').html('');
+		if($('#restaurantdescription').val().length>0 && $('#restaurantname').val().length>0 && $('#restaurantaddress').val().length>0 
+		    	&& $('#restaurantemail').val().length>0 && $('#password_restaurant').val() == $('#confirm_password_restaurant').val()
+		    	&& $('#confirm_password_restaurant').val().length>=6){
+			document.getElementById('sendrestaurant').className = "btnMore";
+			document.getElementById('sendrestaurant').removeAttribute('disabled');
+			$('#message_restaurant_button').html('');
+		}
+	}
+});
+
+
+function isNumber(n) {
+	return !isNaN(parseFloat(n)) && isFinite(n);
+}
 
 $('#password_client, #confirm_password_client').on('keyup', function () {
 	if($('#password_client').val().length>=6){
 		if ($('#password_client').val() !== $('#confirm_password_client').val()&&$('#confirm_password_client').val().length>=6) {
 			$('#message_client').html('Passwords not matching').css('color', 'red');
-	    } else{
+			document.getElementById("senduser").disabled = true;
+			$('#message_user_button').html('Fill the form before register');
+			document.getElementById('senduser').className = "logInBtn";
+	    } else if($('#confirm_password_client').val().length<6){
+			document.getElementById("senduser").disabled = true;
+			$('#message_user_button').html('Fill the form before register');
+	    	document.getElementById('senduser').className = "logInBtn";
+	    }else{
 	    	$('#message_client').html('');
+	    	if($('#userdescriptionform').val().length>0 && $('#usernameform').val().length>0 && $('#usernameform').val().length>0){
+	    		if(isNumber($('#userageform').val())){
+	    			document.getElementById('senduser').className = "btnMore";
+	    			document.getElementById('senduser').removeAttribute('disabled');
+	    			$('#message_user_button').html('');
+	    		}
+	    			
+	    	} 	
 	    }
 	}
 });
+
+$('#userageform').on('keyup', function () {
+	if($('#userageform').val().length==0 || !isNumber($('#userageform').val())){
+		document.getElementById("senduser").disabled = true;
+		$('#message_user_age').html('Age must be nuberic').css('color', 'red');
+		$('#message_user_button').html('Fill the form before register');
+		document.getElementById('senduser').className = "logInBtn";
+	}else{
+		$('#message_user_age').html('');
+		if($('#userdescriptionform').val().length>0 && $('#usernameform').val().length>0 && $('#usernameform').val().length>0 
+		    	&& $('#password_client').val() == $('#confirm_password_client').val()
+		    	&& $('#confirm_password_client').val().length>=6){
+			document.getElementById('senduser').className = "btnMore";
+			document.getElementById('senduser').removeAttribute('disabled');
+			$('#message_user_button').html('');
+		}
+	}
+});
+
+
+
 
 let oldDay="13";
 function selectDay(newDay) {
