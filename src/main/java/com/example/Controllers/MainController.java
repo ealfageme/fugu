68d679bdc0bad4690b4514e96cbb73e1f84d0a48
HDominguezD <hector_sdr.rm_vk@hotmail.com>
2017-03-15 16:57:11 +0100

    main

--- src/main/java/com/example/Controllers/MainController.java
+++ src/main/java/com/example/Controllers/MainController.java
@@ -1,7 +1,10 @@
 package com.example.Controllers;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,7 +24,7 @@ public class MainController {
 	private CityRepository cityRepository;
 
 	@RequestMapping("/main/")
-	public String main(Model model, Restaurant restaurant, @RequestParam(required=false) String restaurantname, 
+	public String main(Model model, HttpServletRequest request, Authentication authentication, Restaurant restaurant, @RequestParam(required=false) String restaurantname, 
 			 @RequestParam(required=false) String restaurantaddress,@RequestParam(required=false) String kindoffood,
 			 @RequestParam(required=false) String restaurantcity,@RequestParam(required=false) String restaurantemail,
 			 @RequestParam(required=false) String restaurantphone,@RequestParam(required=false) String restaurantdescription,
@@ -30,7 +33,6 @@ public class MainController {
 			@RequestParam(required=false) String favouritefood,@RequestParam(required=false) String userdescription,
 			@RequestParam(required=false) String userpassword) {	
 		model.addAttribute("restaurant", restaurantRepository.findByRateBetweenOrderByRateDesc(new Double(0.0), new Double(5.0), new PageRequest(0, 4)));
-		System.out.println(restaurantname);
 		if (restaurantname!=null){
 			Restaurant rest= new Restaurant (restaurantname,restaurantaddress,restaurantdescription,restaurantemail,kindoffood,Integer.parseInt(restaurantphone), 0, 0,restaurantpassword,true,true,true,"ROLE_RESTAURANT");
 			rest.setCity(cityRepository.findByName(restaurantcity));
@@ -40,6 +42,9 @@ public class MainController {
 			User user = new User(username,useremail,userdescription, userpassword ,Integer.parseInt(userage),favouritefood,"ROLE_USER");
 			userRepository.save(user);
 		}
+		model.addAttribute("inSession", (request.isUserInRole("USER")||request.isUserInRole("RESTAURANT")));
+		model.addAttribute("outSession", !request.isUserInRole("USER")&&!request.isUserInRole("RESTAURANT"));
+		
 		return "main";
 	}
 
