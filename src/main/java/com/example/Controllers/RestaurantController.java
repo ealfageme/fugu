609da6828fcf6c67d35ee930d5562af3edb5f2a1
ealfageme <e.alfageme@alumnos.urjc.es>
2017-03-18 23:01:03 +0100

    specify age in offers restaurant and miscellaneous changes

--- src/main/java/com/example/Controllers/RestaurantController.java
+++ src/main/java/com/example/Controllers/RestaurantController.java
@@ -4,7 +4,7 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.List;
+
 
 import javax.servlet.http.HttpSession;
 
