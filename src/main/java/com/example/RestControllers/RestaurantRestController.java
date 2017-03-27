3040b08e6fdd165bf9fd30726aa8a38e75702d08
srivasd <s.rivasd@alumnos.urjc.es>
2017-03-27 11:32:05 +0200

    Rest controllers updated

--- src/main/java/com/example/RestControllers/RestaurantRestController.java
+++ src/main/java/com/example/RestControllers/RestaurantRestController.java
@@ -8,15 +8,18 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.Entities.Menu;
 import com.example.Entities.Restaurant;
 import com.example.Repositories.MenuRepository;
 import com.example.Repositories.RestaurantRepository;
+import com.fasterxml.jackson.annotation.JsonView;
 
 @RestController
 public class RestaurantRestController {
@@ -25,29 +28,50 @@ public class RestaurantRestController {
 	private RestaurantRepository restaurantRepository;
 	@Autowired
 	private MenuRepository menuRepository;
-	
-	
+
 	@ResponseBody
 	@RequestMapping(value = "/api/restaurants/{id}", method = RequestMethod.GET)
 	public ResponseEntity<Restaurant> getRestaurant(HttpSession session, @PathVariable long id) {
 		session.setMaxInactiveInterval(-1);
-		return new ResponseEntity<>(restaurantRepository.findOne(id), HttpStatus.OK);
+		Restaurant rest = restaurantRepository.findOne(id);
+		if (rest != null) {
+			return new ResponseEntity<>(restaurantRepository.findOne(id), HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
 	}
 
-	@RequestMapping(value="/api/restaurants/{id}/menus", method=RequestMethod.GET)
+	@RequestMapping(value = "/api/restaurants/{id}/menus", method = RequestMethod.GET)
 	public ResponseEntity<Page<Menu>> getRestaurantMenus(HttpSession session, @PathVariable long id, Pageable page) {
 		session.setMaxInactiveInterval(-1);
-		Restaurant restaurant=restaurantRepository.findOne(id);
-		return new ResponseEntity<>(menuRepository.findByRestaurantMenu(restaurant,page), HttpStatus.OK);
+		Restaurant restaurant = restaurantRepository.findOne(id);
+		if (restaurant != null) {
+			return new ResponseEntity<>(menuRepository.findByRestaurantMenu(restaurant, page), HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+
+	}
+
+	@ResponseBody
+	@RequestMapping(value = "/api/restaurants/", method = RequestMethod.GET)
+	public ResponseEntity<Page<Restaurant>> getRestaurants(HttpSession session, Pageable page) {
+		session.setMaxInactiveInterval(10);
+		Page<Restaurant> restaurants = restaurantRepository.findByRateBetweenOrderByRateDesc(new Double(0.0),
+				new Double(5.0), page);
+		if (restaurants != null) {
+			return new ResponseEntity<>(restaurants, HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
 	}
 	
 	@ResponseBody
-	@RequestMapping(value="/api/restaurants/", method=RequestMethod.GET)
-	public ResponseEntity<Page<Restaurant>> getRestaurants(HttpSession session, Pageable page) {
-		session.setMaxInactiveInterval(10);
-
-		return new ResponseEntity<>(
-				restaurantRepository.findByRateBetweenOrderByRateDesc(new Double(0.0), new Double(5.0), page),
-				HttpStatus.OK);
+	@JsonView(Restaurant.Basic.class)
+	@RequestMapping(value = "/api/restauants/", method = RequestMethod.POST)
+	@ResponseStatus(HttpStatus.CREATED)
+	public ResponseEntity<Restaurant> addRestaurant(@RequestBody Restaurant restaurant, HttpSession session) {
+		session.setMaxInactiveInterval(-1);
+		return new ResponseEntity<>(restaurant, HttpStatus.OK);
 	}
 }
