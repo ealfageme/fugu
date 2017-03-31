725c28085e2fe493245a2339e393c54dd0a7d7d0
srivasd <s.rivasd@alumnos.urjc.es>
2017-03-31 12:30:20 +0200

    deserializer

--- src/main/java/com/example/UserFollowers.java
+++ src/main/java/com/example/UserFollowers.java
@@ -3,6 +3,7 @@ package com.example;
 import java.io.IOException;
 
 import org.springframework.boot.jackson.JsonComponent;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import com.example.Entities.Booking;
 import com.example.Entities.Restaurant;
@@ -10,8 +11,13 @@ import com.example.Entities.Review;
 import com.example.Entities.User;
 import com.example.Entities.Voucher;
 import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.ObjectCodec;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
 
@@ -40,6 +46,8 @@ public class UserFollowers {
 			jgen.writeNumberField("id", user.getId());
 			jgen.writeStringField("name", user.getName());
 			jgen.writeStringField("email", user.getEmail());
+			jgen.writeObjectField("password", user.getPassword());
+			jgen.writeObjectField("roles", user.getRoles());
 			jgen.writeNumberField("age", user.getAge());
 			jgen.writeStringField("description", user.getDescription());
 			jgen.writeStringField("favouriteFood", user.getFavouriteFood());
@@ -79,7 +87,7 @@ public class UserFollowers {
 				jgen.writeNumberField("id", booking.getId());
 				jgen.writeNumberField("number", booking.getNumber());
 				jgen.writeStringField("specialRequirements", booking.getSpecialRequirements());
-				jgen.writeStringField("date", booking.getDate().toString());
+				jgen.writeObjectField("date", booking.getDate());
 				jgen.writeEndObject();
 			}
 			jgen.writeEndArray();
@@ -88,7 +96,7 @@ public class UserFollowers {
 			for(Review review: user.getReviews()){
 				jgen.writeStartObject();
 				jgen.writeNumberField("id", review.getId());
-				jgen.writeStringField("date", review.getDate().toString());
+				jgen.writeObjectField("date", review.getDate());
 				jgen.writeStringField("content", review.getContent());
 				jgen.writeNumberField("rate", review.getRate());
 				jgen.writeEndObject();
@@ -96,4 +104,34 @@ public class UserFollowers {
 			jgen.writeEndArray();
 		}
     }
+	/*public static class UserDeserializer extends JsonDeserializer<User> {
+
+		@Override
+		public final User deserialize(JsonParser jp, DeserializationContext ctxt)
+				throws IOException {
+			try {
+				ObjectCodec codec = jp.getCodec();
+				JsonNode tree = codec.readTree(jp);
+				return deserializeObject(jp, ctxt, codec, tree);
+			}
+			catch (Exception ex) {
+				if (ex instanceof IOException) {
+					throw (IOException) ex;
+				}
+				throw new JsonMappingException(jp, "Object deserialize error", ex);
+			}
+		}
+
+		private User deserializeObject(JsonParser jp, DeserializationContext ctxt, ObjectCodec codec, JsonNode tree) throws IOException {
+			JsonNode node = jp.getCodec().readTree(jp);
+			String name = node.get("name").asText();
+			String email = node.get("email").asText();
+			String description = node.get("description").asText();
+			String password = node.get("password").asText();
+			int age = (int) node.get("age").numberValue();
+			String favouriteFood = node.get("favouriteFood").asText();
+			return new User(name, email, description, password, age, favouriteFood);
+		}
+
+	}*/
 }
