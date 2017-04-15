3ddda408f53879c0355a2ff1f164791f4ccaa092
sergiopaniego <sergiopaniegoblanco@gmail.com>
2017-04-15 18:20:05 +0200

    Kicking off Angular

--- src/main/java/com/example/FuguApplication.java
+++ src/main/java/com/example/FuguApplication.java
@@ -2,11 +2,25 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class FuguApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FuguApplication.class, args);
+	}
+ 
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+		public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
 	}
 }
