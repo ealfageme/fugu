2e90576a92cd50a437445539708d80e9fc91059a
srivasd <s.rivasd@alumnos.urjc.es>
2017-04-17 09:56:42 +0200

    CORS substitution

--- src/main/java/com/example/FuguApplication.java
+++ src/main/java/com/example/FuguApplication.java
@@ -18,9 +18,12 @@ public class FuguApplication {
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurerAdapter() {
             @Override
-		public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("*");
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "OPTIONS", "PUT", "DELETE")
+                .allowedHeaders("Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method", "Access-Control-Request-Headers")
+                .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials")
+                .allowCredentials(true).maxAge(3600);
             }
         };
-	}
+    }
 }
