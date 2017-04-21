05640b5d9a7f02cb6bbd978c1f0665bf576182a5
ealfageme <e.alfageme@alumnos.urjc.es>
2017-04-21 12:13:51 +0200

    reduce search a one button

--- fugu/src/app/search-web/search-web.component.ts
+++ fugu/src/app/search-web/search-web.component.ts
@@ -11,24 +11,22 @@ export class SearchWebComponent implements OnInit {
   inSession: boolean;
   facebookSession: boolean;
   private restaurants: string[] = [];
+  private restaurantsByPriceAndRating: string[] = [];
   email: string;
   password: string;
   maxPrice = 25;
   minPrice = 1;
   minRating = 0;
   maxRating = 5;
-  city: String;
-  typefood: String;
+  city = "Madrid";
+  typefood = "All";
   nameRestaurant: String;
 
 
   constructor(private http: Http) {
     this.inSession = false;
-    //dates as default
-    this.city = "Madrid";
-    this.typefood = "Chinese";
     this.facebookSession = false;
-    
+
 
     this.http.get('https://localhost:8443/api/restaurants/?page=').subscribe(
       response => {
@@ -50,39 +48,53 @@ export class SearchWebComponent implements OnInit {
 
   searchByParam() {
     this.restaurants = [];
+    this.restaurantsByPriceAndRating = [];
     this.http.get('https://localhost:8443/api/search-web/filters?min=' + this.minRating + '&&max=' + this.maxRating + '&&minPrice=' + this.minPrice + '&&maxPrice=' + this.maxPrice).subscribe(
       response => {
         const data = response.json();
         for (let i = 0; i < data.length; i++) {
           const restaurant = data[i];
-          this.restaurants.push(restaurant);
+          this.restaurantsByPriceAndRating.push(restaurant);
         }
       },
       error => console.error(error)
     );
+    if (this.typefood !== "All") {
+      this.http.get('https://localhost:8443/api/search-web/foodtypeandcity?typeFood=' + this.typefood + '&&city=' + this.city).subscribe(
+        response => {
+          const data = response.json();
+          for (let i = 0; i < data.length; i++) {
+            const restaurant = data[i];
+            this.restaurants.push(restaurant);
+          }
+        },
+        error => console.error(error)
+      );
+    } else { // find by city (only)
+      this.http.get('https://localhost:8443/api/city/' + this.city).subscribe(
+        response => {
+          const data = response.json();
+          for (let i = 0; i < data.length; i++) {
+            const restaurant = data[i];
+            this.restaurants.push(restaurant);
+          }
+        },
+        error => console.error(error)
+      );
+
+    }
+    this.intersection();
   }
-  searchByCityAndType() {
-    this.restaurants = [];
-    
-    this.http.get('https://localhost:8443/api/search-web/foodtypeandcity?typeFood=' + this.typefood + '&&city=' + this.city).subscribe(
-      response => {
-        const data = response.json();
-        for (let i = 0; i < data.length; i++) {
-          const restaurant = data[i];
-          this.restaurants.push(restaurant);
-        }
-      },
-      error => console.error(error)
-    );
-  }
-  searchByName() {
-    this.restaurants = [];
-    this.http.get('https://localhost:8443/api/search-web/name?name=' + this.nameRestaurant).subscribe(
-      response => {
-        const restaurant = response.json();
-        this.restaurants.push(restaurant);
-      },
-      error => console.error(error)
-    );
-  }
+intersection(){}
+
+searchByName() {
+  this.restaurants = [];
+  this.http.get('https://localhost:8443/api/search-web/name?name=' + this.nameRestaurant).subscribe(
+    response => {
+      const restaurant = response.json();
+      this.restaurants.push(restaurant);
+    },
+    error => console.error(error)
+  );
+}
 }
