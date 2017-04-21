2c720fc5b997dc3475c9c345cd193f2e2a816b53
sergiopaniego <sergiopaniegoblanco@gmail.com>
2017-04-21 11:52:55 +0200

    Code cleaned

--- fugu/src/app/public-client/public-client.component.ts
+++ fugu/src/app/public-client/public-client.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import  {  Http  }  from  '@angular/http';
-import  {  Router, ActivatedRoute  }  from  '@angular/router';
+import { Http } from '@angular/http';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-public-client',
@@ -12,59 +12,20 @@ export class PublicClientComponent implements OnInit {
   private inSession: boolean;
   private followButton: boolean;
   private unfollowButton: boolean;
-  private  restaurants: string[] = [];
-  private  following: string[] = [];
+  private restaurants: string[] = [];
+  private following: string[] = [];
   private reviews: string[] = [];
   private user: string;
   private username: string;
   params: any;
 
 
-  constructor(private  http: Http, activatedRoute: ActivatedRoute, private router: Router) {
+  constructor(private http: Http, activatedRoute: ActivatedRoute, private router: Router) {
     this.params = activatedRoute.params.subscribe(
       params => {
-        this.username = params['username'];
-        console.log(this.username); // this consoles the correct true/false value
-        this.inSession = false;
-        this.followButton = false;
-        this.unfollowButton = true;
-        this.restaurants.splice(0);
-        this.http.get('https://localhost:8443/api/restaurants/?page=0&size=4').subscribe(
-          response => {
-            const data = response.json();
-            for (let i = 0; i < data.content.length; i++)  {
-              const restaurant = data.content[i];
-              this.restaurants.push(restaurant);
-            }
-          },
-          error => console.error(error)
-        );
-        this.following.splice(0);
-        this.http.get('https://localhost:8443/api/clients/').subscribe(
-          response => {
-            const data = response.json();
-            for (let i = 0; i < data.content.length; i++) {
-              const follow = data.content[i];
-              this.following.push(follow);
-            }
-          },
-          error => console.error(error)
-        );
-        this.reviews.splice(0);
-        this.http.get('https://localhost:8443/api/clients/' + this.username).subscribe(
-          response => {
-            const data = response.json();
-            for (let i = 0; i < data.reviews.length; i++)  {
-              const review = data.reviews[i];
-              this.reviews.push(review);
-            }
-          this.user = data;
-          },
-          error => console.error(error)
-        );
+        this.binding(params);
       }
     );
-
   }
 
   ngOnInit() {
@@ -72,5 +33,58 @@ export class PublicClientComponent implements OnInit {
 
   goTo(location: string): void {
     window.location.hash = location;
+  }
+
+  binding(params) {
+    this.username = params['username'];
+    this.inSession = false;
+    this.followButton = false;
+    this.unfollowButton = true;
+    this.getRestaurants();
+    this.getFollowers();
+    this.getReviews();
+  }
+
+  getRestaurants() {
+    this.restaurants.splice(0);
+    this.http.get('https://localhost:8443/api/restaurants/?page=0&size=4').subscribe(
+      response => {
+        const  data = response.json();
+        for (let i = 0; i < data.content.length; i++) {
+          const  restaurant = data.content[i];
+          this.restaurants.push(restaurant);
+        }
+      },
+      error => console.error(error)
+    );
+  }
+
+  getFollowers() {
+    this.following.splice(0);
+    this.http.get('https://localhost:8443/api/clients/').subscribe(
+      response => {
+        const  data = response.json();
+        for (let i = 0; i < data.content.length; i++) {
+          const  follow = data.content[i];
+          this.following.push(follow);
+        }
+      },
+      error => console.error(error)
+    );
+  }
+
+  getReviews() {
+    this.reviews.splice(0);
+    this.http.get('https://localhost:8443/api/clients/' + this.username).subscribe(
+      response => {
+        const  data = response.json();
+        for (let i = 0; i < data.reviews.length; i++) {
+          const  review = data.reviews[i];
+          this.reviews.push(review);
+        }
+        this.user = data;
+      },
+      error => console.error(error)
+    );
   }
 }
